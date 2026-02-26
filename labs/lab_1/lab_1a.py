89aa8c6e8c11492f3e9922fd0cbad5b0dc61a1e6
Seungwan Han <superseungwan228@gmail.com>
2026-02-26 16:07:56 -0600

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Seungwan Han" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi everyone! My name is Seungwan Han, and I'm a junior. I live in Memphis, Tennessee, but I go to a boarding school in Minnesota. The reason I board is because I play soccer for an MLS NEXT team there. I'm into math, CAD, and 3D printing, and in my free time I like to read mysteries, play Brawl Stars, and eat ice cream. After watching the Olympics, I'm also thinking of getting into competitive curling and am in the process of searching for potential partners (so let me know if you're interested). Looking forward to working with you!")
 if __name__ == "__main__":
     main()
